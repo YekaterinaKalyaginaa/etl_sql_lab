7310585fa5505a5c7a6e88e55ab9783e5b74dd9a
YekaterinaKalyaginaa <yekaterinakalyagina1937@gmail.com>
2025-12-20 05:12:46 +0600

    Update try_parse_date function to restrict date formats
    Removed support for DD.MM.YYYY format in date parsing.

--- sql/dds/s_psql_dds/function/try_parse_date.sql
+++ sql/dds/s_psql_dds/function/try_parse_date.sql
@@ -1,3 +1,7 @@
+create schema if not exists s_psql_dds;
+
+drop function if exists s_psql_dds.try_parse_date(text);
+
 create or replace function s_psql_dds.try_parse_date(p_text text)
 returns date
 language plpgsql
@@ -9,11 +13,13 @@ begin
     return null;
   end if;
 
+  --  Разрешаем ТОЛЬКО:
+  -- 1) YYYY-MM-DD
+  -- 2) DD/MM/YYYY
+  --  Формат DD.MM.YYYY (с точками) НЕ рассматриваем вообще (ДА НЕ МОГУ Я ЭТО СДЕЛАТЬ БЛИИИН)
   begin
     if p_text ~ '^\d{4}-\d{2}-\d{2}$' then
       d := to_date(p_text, 'YYYY-MM-DD');
-    elsif p_text ~ '^\d{2}\.\d{2}\.\d{4}$' then
-      d := to_date(p_text, 'DD.MM.YYYY');
     elsif p_text ~ '^\d{2}/\d{2}/\d{4}$' then
       d := to_date(p_text, 'DD/MM/YYYY');
     else
