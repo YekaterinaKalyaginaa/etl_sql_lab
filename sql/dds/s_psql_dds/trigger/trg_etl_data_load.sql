896cb495fbb894ee8c94707d241197936428707d
YekaterinaKalyaginaa <yekaterinakalyagina1937@gmail.com>
2025-12-20 05:13:46 +0600

    Refactor trg_etl_data_load function and trigger
    Removed the function and trigger definitions for trg_etl_data_load, and updated the date parsing logic in the function.

--- sql/dds/s_psql_dds/trigger/trg_etl_data_load.sql
+++ sql/dds/s_psql_dds/trigger/trg_etl_data_load.sql
@@ -1,25 +1,5 @@
-drop function if exists s_psql_dds.trg_etl_data_load();
-drop trigger if exists trg_etl_data_load on s_psql_dds.t_sql_source_unstructured;
-
-create or replace function s_psql_dds.trg_etl_data_load()
-returns trigger
-language plpgsql
-as $$
-declare
-  v_dt date;
-begin
-  v_dt := s_psql_dds.try_parse_date(new.signup_date);
-  if v_dt is null then
-    v_dt := current_date;
-  end if;
-
-  perform s_psql_dds.fn_etl_data_load(v_dt, v_dt);
-
-  return new;
-end;
-$$;
-
-create trigger trg_etl_data_load
-after insert on s_psql_dds.t_sql_source_unstructured
-for each row
-execute function s_psql_dds.trg_etl_data_load();
+v_dt := s_psql_dds.try_parse_date(new.signup_date::text);
+if v_dt is null then
+  v_dt := current_date;
+end if;
+perform s_psql_dds.fn_etl_data_load(v_dt, v_dt);
