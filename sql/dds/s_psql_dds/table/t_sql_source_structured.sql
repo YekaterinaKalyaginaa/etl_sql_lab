eba34d438cf437ca2f2e8be663e6543bf54aa70c
YekaterinaKalyaginaa <yekaterinakalyagina1937@gmail.com>
2025-12-20 04:53:48 +0600

    Modify customer_id to be not null and add primary key
    Added 'not null' constraint to customer_id and defined primary key.

--- sql/dds/s_psql_dds/table/t_sql_source_structured.sql
+++ sql/dds/s_psql_dds/table/t_sql_source_structured.sql
@@ -1,7 +1,7 @@
 drop table if exists s_psql_dds.t_sql_source_structured;
 
 create table s_psql_dds.t_sql_source_structured (
-    customer_id     integer,
+    customer_id     integer not null,
     full_name       text,
     gender          text,
     age             integer,
@@ -10,5 +10,6 @@ create table s_psql_dds.t_sql_source_structured (
     monthly_income  numeric(12,2),
     signup_date     date,
     valid_from      date,
-    valid_to        date
+    valid_to        date,
+    constraint t_sql_source_structured_pk primary key (customer_id)
 );
